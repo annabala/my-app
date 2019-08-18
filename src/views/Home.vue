1bdd26606faa7ec6f74ddd94b209020ce0f410d0
[Anna Bała] <[a.bala@cormo.pl]>
2019-08-18 21:46:44 +0200

    working on Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div class="shapeContainer">
+        <div class="shapeContainer__item"></div>
+        <div class="shapeContainer__item"><p>jQuery</p></div>
+        <div class="shapeContainer__item"><p>sass</p></div>
+        <div class="shapeContainer__item"><p>html</p></div>
+        <div class="shapeContainer__item"><p>vue.js</p></div>
+        <div class="shapeContainer__item"></div>
+        <div class="shapeContainer__item"><p>webpack</p></div>
+        <div class="shapeContainer__item"><p>git</p></div>
+        <div class="shapeContainer__item"><p>materialize</p></div>
+        <div class="shapeContainer__item"><p>bootstrap</p></div>
+        <div class="shapeContainer__item"><p>angular</p></div>
+        <div class="shapeContainer__item"><p>javascript</p></div>
+    </div>
   </div>
 </template>
 
@@ -11,3 +25,6 @@ export default {
   name: 'home',
 };
 </script>
+<style lang="scss" scoped>
+@import "../styles/views/home.scss";
+</style>
