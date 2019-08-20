41ac38be8b8cf68e13828b0447f6da70bd76b98b
[Anna Bała] <[a.bala@cormo.pl]>
2019-08-20 22:34:06 +0200

    styling front page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <div class="shapeContainer">
-        <div class="shapeContainer__item"></div>
-        <div class="shapeContainer__item"><p>jQuery</p></div>
-        <div class="shapeContainer__item"><p>sass</p></div>
-        <div class="shapeContainer__item"><p>html</p></div>
-        <div class="shapeContainer__item"><p>vue.js</p></div>
-        <div class="shapeContainer__item"></div>
-        <div class="shapeContainer__item"><p>webpack</p></div>
-        <div class="shapeContainer__item"><p>git</p></div>
-        <div class="shapeContainer__item"><p>materialize</p></div>
-        <div class="shapeContainer__item"><p>bootstrap</p></div>
-        <div class="shapeContainer__item"><p>angular</p></div>
-        <div class="shapeContainer__item"><p>javascript</p></div>
+    <div class="container">
+      <h1 class="h1">Front end Developer</h1>
+      <div class="shapeContainer">
+        <div class="shapeContainer__block">
+            <div class="shapeContainer__block__item"><p></p></div>
+            <div class="shapeContainer__block__item"><p>jQuery</p></div>
+            <div class="shapeContainer__block__item"><p>sass</p></div>
+            <div class="shapeContainer__block__item"><p>html</p></div>
+        </div>
+        <div class="shapeContainer__block">
+          <div class="shapeContainer__block__item"><p>vue.js</p></div>
+          <div class="shapeContainer__block__item"><p></p></div>
+          <div class="shapeContainer__block__item"><p>webpack</p></div>
+          <div class="shapeContainer__block__item"><p>git</p></div>
+        </div>
+        <div class="shapeContainer__block">
+          <div class="shapeContainer__block__item"><p>materialize</p></div>
+          <div class="shapeContainer__block__item"><p>bootstrap</p></div>
+          <div class="shapeContainer__block__item"><p>angular</p></div>
+          <div class="shapeContainer__block__item"><p>javascript</p></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
